b4e00ae8278aa2c6f8bc4906b39aba847c87e186
chetantulluru-lab <chetantulluru@gmail.com>
2026-02-14 21:53:08 +0530

    Create 2_insert_data.sql

---
+++ week1(SQL_basics)/2_insert_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO students VALUES (1, 'Rahul', 20, 85);
+INSERT INTO students VALUES (2, 'Anita', 21, 92);
+INSERT INTO students VALUES (3, 'John', 19, 70);
+INSERT INTO students VALUES (4, 'Priya', 22, 60);
+INSERT INTO students VALUES (5, 'Arjun', 20, 88);
