873fa3d9d28ea18467ed36bdaf5323b2a673bf58
chetantulluru-lab <chetantulluru@gmail.com>
2026-02-14 21:36:58 +0530

    Create grade_calculator.py

---
+++ Week1(python_basics)/conditions/grade_calculator.py
@@ -0,0 +1,10 @@
+# Calculate grade based on marks
+marks = int(input("Enter marks: "))
+if marks >= 90:
+    print("Grade: A")
+elif marks >= 75:
+    print("Grade: B")
+elif marks >= 50:
+    print("Grade: C")
+else:
+    print("Grade: F")
