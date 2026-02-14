5100e7e0a83986cd4e0430eff36ab009bf044ea7
chetantulluru-lab <chetantulluru@gmail.com>
2026-02-14 21:33:29 +0530

    Create sum_of_digits.py

---
+++ Week1(python_basics)/while_loops/sum_of_digits.py
@@ -0,0 +1,8 @@
+# Sum of digits of a number
+num = int(input("Enter a number: "))
+total = 0
+while num > 0:
+    digit = num % 10
+    total += digit
+    num //= 10
+print("Sum of digits =", total)
