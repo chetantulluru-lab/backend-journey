d51c2f19c5ab194cfba0723378c618642ca1348e
chetantulluru-lab <chetantulluru@gmail.com>
2026-02-14 21:00:28 +0530

    Delete week1-python-basics/loops directory

--- week1-python-basics/loops/multiplication_table.py
+++
@@ -1,5 +0,0 @@
-# multiplication_table.py
-x = int(input("Enter a number: "))
-
-for i in range(1, 11):
-    print(x, "x", i, "=", x * i)
--- week1-python-basics/loops/print_numbers_1_to_10.py
+++
@@ -1,5 +0,0 @@
-# print_numbers_1_to_10.py
-i = 1
-while i <= 10:
-    print(i)
-    i += 1
